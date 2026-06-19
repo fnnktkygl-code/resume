81d49078f1d833d69932922bd3896a71b75ced4c
fnnk <fnnktkygl@gmail.com>
2026-06-19 11:36:19 +0200

    fix: import useRef in CVManagerModal to resolve crash when clicking Gérer mes CVs

--- src/components/ui/CVManagerModal.jsx
+++ src/components/ui/CVManagerModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Modal from './Modal';
 import { useTranslation } from '../../utils/TranslationContext';
 
